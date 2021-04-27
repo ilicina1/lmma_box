b71096848554832f3f967c823086d15c55b8d6db
ilicina1 <ilhan.licina@outlook.com>
2021-04-27 16:44:43 +0200

    commit pred merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:lmma_box/view/welcome_screen/pages/splash_screen.dart';
-import 'package:lmma_box/providers/screen_scroll.dart';
-import 'package:provider/provider.dart';
 
 void main() {
-  runApp(ChangeNotifierProvider(
-    create: (context) => ScreenScrollProvider(0),
-    child: MyApp(),
-  ));
-  SystemChrome.setSystemUIOverlayStyle(
-      SystemUiOverlayStyle(statusBarColor: Colors.white));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -18,13 +9,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'LmmaBox',
       theme: ThemeData(
-        fontFamily: 'Averta',
         primarySwatch: Colors.blue,
       ),
-      home: SplashApp(),
+      home: MyHomePage(),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
