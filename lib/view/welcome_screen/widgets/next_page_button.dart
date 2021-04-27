c04893b8c3f2bc0cc6dde328e524ef5a9253c68a
amer460 <amer.zlatic.az@gmail.com>
2021-04-27 15:29:59 +0200

    commit pred merge

--- lib/view/welcome_screen/widgets/next_page_button.dart
+++ lib/view/welcome_screen/widgets/next_page_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lmma_box/utils/style/next_button_style.dart';
-import 'package:lmma_box/utils/style/first_page_text_styles.dart';
+import 'package:lmma_box/utils/style/welcome_screen_text_styles.dart';
 
 Widget nextPageButton() {
   return SizedBox(
