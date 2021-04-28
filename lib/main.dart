7eefdb0d238096891fe8936a2f090fea149a5148
ilicina1 <ilhan.licina@outlook.com>
2021-04-28 11:21:22 +0200

    merge sa ilhan

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lmma_box/providers/screen_scroll.dart';
-import 'package:lmma_box/view/welcome_screen/pages/splash_screen.dart';
+import 'package:lmma_box/view/loading_screen/pages/splash_screen.dart';
 import 'package:provider/provider.dart';
 
 void main() {
