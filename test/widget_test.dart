af9aee580a6440a9ce9e6fcdfb22de9bb13b5cc6
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-28 14:34:14 +0200

    dodani testovi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -15,19 +15,30 @@ import 'package:lmma_box/view/welcome_screen/pages/first_page.dart';
 import 'package:lmma_box/view/welcome_screen/pages/page_scroller.dart';
 import 'package:lmma_box/view/welcome_screen/widgets/first_page_widgets/set_text_welcome_screen.dart';
 import 'package:lmma_box/view/welcome_screen/widgets/second_page_widgets/app_icon.dart';
+import 'package:lmma_box/view/welcome_screen/widgets/shared_widgets/welcome_screen_pagination.dart';
 
 void main() {
-  testWidgets('Ne postoji ovakav title', (WidgetTester tester) async {
-    await tester.pumpWidget(PageScroller());
+  Widget createWidgetForTesting({Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
+
+  testWidgets('Koristi se Scaffold', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new FirstPage()));
     var textField = find.byType(Scaffold);
     expect(textField, findsOneWidget);
-/*
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);*/
+  testWidgets('Naslov je dobar', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new FirstPage()));
+    var textField = find.text("Welcome to LmaaBox");
+    expect(textField, findsOneWidget);
+  });
+
+  testWidgets('Postoji logo firme', (WidgetTester tester) async {
+    await tester.pumpWidget(createWidgetForTesting(child: new FirstPage()));
+    var textField = find.byType(Image);
+    expect(textField, findsOneWidget);
   });
 }
