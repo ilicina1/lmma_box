f694908e968208f76ba6e5d3a85e82a88c5b5079
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-30 16:36:36 +0200

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:lmma_box/viewModel/facebooksigninViewModel.dart';
-import 'package:lmma_box/viewModel/googlesigninViewModel.dart';
-
-import 'buttons.dart';
+import 'package:flutter/services.dart';
+import 'package:lmma_box/providers/screen_scroll.dart';
+import 'package:lmma_box/view/loading_screen/pages/splash_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ScreenScrollProvider(0),
+    child: MyApp(),
+  ));
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.white));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(title: 'AWS Cognito Google Facebook signin'),
+      debugShowCheckedModeBanner: false,
+      title: 'LmmaBox',
+      theme: ThemeData(
+        fontFamily: 'Averta',
+        primarySwatch: Colors.blue,
+      ),
+      home: SplashApp(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  String idendity_provider;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
-            child: Wrap(
-              runSpacing: 20,
-              children: [
-                SizedBox(
-                    width: double.infinity,
-                    child: GoogleButton(
-                        GoogleSignInViewModel().onGoogleSignIn(context))),
-                SizedBox(
-                    width: double.infinity,
-                    child: FacebookButton(
-                        FacebookSignInViewModel().facebookLogin(context))),
-              ],
-            ),
-          ),
-        ));
   }
 }
