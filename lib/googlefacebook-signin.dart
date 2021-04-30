18c7da57cdab0eaf9a6e947af2eb10c9508a3c61
Faris Čolaković <fcolakovic2@etf.unsa.ba>
2021-04-30 15:48:25 +0200

    refaktorisan kod

--- lib/googlefacebook-signin.dart
+++ lib/googlefacebook-signin.dart
@@ -71,50 +71,6 @@ class WebViewGoogleFacebookState extends State<WebViewGoogleFacebook> {
     );
   }
 
-  Future signUserInWithAuthCode(String authCode) async {
-    String url = "https://${COGNITO_POOL_URL}" +
-        ".amazoncognito.com/oauth2/token?grant_type=authorization_code&client_id=" +
-        "${COGNITO_CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=" +
-        authCode +
-        "&redirect_uri=myapp://";
-    final response = await http.post(Uri.parse(url),
-        body: {},
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'});
-    if (response.statusCode != 200) {
-      throw Exception("Received bad status code from Cognito for auth code:" +
-          response.statusCode.toString() +
-          "; body: " +
-          response.body);
-    }
-
-    final tokenData = json.decode(response.body);
-
-    final idToken = new CognitoIdToken(tokenData['id_token']);
-    final accessToken = new CognitoAccessToken(tokenData['access_token']);
-    final refreshToken = new CognitoRefreshToken(tokenData['refresh_token']);
-    final session = new CognitoUserSession(idToken, accessToken,
-        refreshToken: refreshToken);
-
-    final userPool = new CognitoUserPool(COGNITO_Pool_ID, COGNITO_CLIENT_ID);
-    final user = new CognitoUser(null, userPool, signInUserSession: session);
-
-    final attributes = await user.getUserAttributes();
-    for (CognitoUserAttribute attribute in attributes) {
-      if (attribute.getName() == "email") {
-        user.username = attribute.getValue();
-        break;
-      }
-    }
-
-    print("login successfully.");
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => WelcomeScreen()),
-    );
-
-    return user;
-  }
-
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
