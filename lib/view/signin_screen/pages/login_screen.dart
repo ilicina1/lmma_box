92038c11ef7f366b1778f939e5f724bb4e6802af
ilicina1 <ilhan.licina@outlook.com>
2021-05-03 22:43:44 +0200

    dodavanje resizeToAvoidBottomInset za login screen

--- lib/view/signin_screen/pages/login_screen.dart
+++ lib/view/signin_screen/pages/login_screen.dart
@@ -21,6 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       key: _scaffoldKey,
       backgroundColor: Colors.white,
       body: Column(
