72b42e3e617aabf2118df94a865bf2b82ee03c83
ilicina1 <ilhan.licina@outlook.com>
2021-05-11 15:31:58 +0200

    ispravljena greska da nakon wellcome screena salje na home scree

--- lib/view/signup_screen/pages/signup_page.dart
+++ lib/view/signup_screen/pages/signup_page.dart
@@ -2,12 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_session/flutter_session.dart';
 import 'package:lmma_box/view/signup_screen/pages/testSignUp.dart';
 import 'package:lmma_box/view/signup_screen/widgets/signup_main.dart';
-import 'package:lmma_box/view/signup_screen/widgets/signup_page_widgets/app_bar_signup.dart';
-import 'package:lmma_box/view/signup_screen/widgets/signup_page_widgets/form_list.dart';
-import 'package:lmma_box/view/signup_screen/widgets/signup_page_widgets/login_rich_text.dart';
-import 'package:lmma_box/view/welcome_screen/pages/page_scroller.dart';
-
-import '../widgets/signup_page_widgets/app_bar_signup.dart';
 
 class SignUpPage extends StatefulWidget {
   @override
@@ -27,7 +21,7 @@ class _SignUpPageState extends State<SignUpPage> {
         future: FlutterSession().get('mail'),
         builder: (context, snapshot) {
           print(snapshot.data);
-          return snapshot.data != false
+          return snapshot.data != false && snapshot.data != null
               ? TestSignUp(snapshot.data)
               : SignUpMain(_scaffoldKey);
         },
